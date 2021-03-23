12669cf73c5861d27220cd151f06160c05212392
bengroves <bengroves15@gmail.com>
2021-03-23 14:20:52 -0400

    commit tree 32259c28473ec1467234f75ad47abbdf4b2bda50

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 class Formatter {
   //add static methods here
+  static capitalize (string) {
+    let x = string.substr(1)
+    let y = string[0].toUpperCase()
+    return y+x
+  }
+
+  static sanitize (str){
+    let bin = []
+    for (let i = 0; i < str.length; i++){
+      if (i != '!@#$%^&*()_+=`1234567890-~|{}[];:,<>./?'){
+        bin.push(i)
+      }
+    }
+    return bin
+  }
+
 }
